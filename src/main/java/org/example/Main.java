8b6a428cc88b789ed0eaf320f080a9d9424b38a1
AleksanderBanasiak <olek.banasiak0@gmail.com>
2023-11-02 19:59:37 +0100

    sprawdzanie brancha dev

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("sprawdzenie deva");
+        System.out.println("okej teraz sprawdzam deva");
     }
 }
