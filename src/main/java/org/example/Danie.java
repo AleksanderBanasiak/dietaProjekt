328838c6d26460fff55caa5f4f3a24237bb7f144
AleksanderBanasiak <olek.banasiak0@gmail.com>
2023-11-04 12:22:32 +0100

    dodanie zabezpieczenia outOfBound do wyboru produku z listy

--- src/main/java/org/example/Danie.java
+++ src/main/java/org/example/Danie.java
@@ -13,54 +13,30 @@ public class Danie {
 
 
     public List<Produkt> wybierzProduktyDoDania(MenadzerProduktow menadzerProduktow){
+        //tworzenie Listy Dania składanjącego się z obiektów produktów
 
         Scanner scanner = new Scanner(System.in);
-
             System.out.println("Który produkt chcesz dodac: ");
             menadzerProduktow.wyswietlWszystkieProdukty();
-
-
             String wybraneProdukty = scanner.nextLine();
             String[] splitWybraneProdukty = wybraneProdukty.split(",");
-
-            //musze dodac sprawdzenie czy kazdy element tablicy stringow istnieje w liscie
-
-            //sprawdzenie czy nie jest ujemny i nie jest wiekszy czy dlugosc wypisywanej listy
             int[] tablicaWybranychProduktow = new int[splitWybraneProdukty.length];
         for (int i = 0; i < splitWybraneProdukty.length; i++) {
             tablicaWybranychProduktow[i] = (Integer.parseInt(splitWybraneProdukty[i])-1);
         }
-
         List<Produkt> listaWybranychProduktow = new ArrayList<>();
         List<Produkt> produkty = menadzerProduktow.getProdukty();
-
-
-
+        int ilosc =produkty.size();
         for (int i = 0; i < tablicaWybranychProduktow.length; i++) {
-            listaWybranychProduktow.add(produkty.get(tablicaWybranychProduktow[i]));
+            if(tablicaWybranychProduktow[i] < 0 || tablicaWybranychProduktow[i] >= ilosc){
+                System.out.println("błąd koniec programu");
+                System.exit(0);
+            }
+        }
+        for (int i = 0; i < tablicaWybranychProduktow.length; i++) {
+                listaWybranychProduktow.add(produkty.get(tablicaWybranychProduktow[i]));
         }
         return listaWybranychProduktow;
-
-
-//        int wyborProduktu  = Integer.parseInt(scanner.nextLine());
-//        wyborProduktu--;
-//
-//        Produkt p = menadzerProduktow.wybierzProdukt(wyborProduktu);
-//        System.out.println("Podaj gramature " + p.getNazwa());
-//        int ileGram = Integer.parseInt(scanner.nextLine());
-//        //metoda tutaj
-//
-//
-//        String wynik=menadzerProduktow.wypiszProdukt(produkty.get(wyborProduktu));
-//
-//        System.out.println(wynik);
-
-
-
-
-
-
-
     }
 
 
